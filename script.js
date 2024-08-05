ddf9add2e0ed42a09408c0bfa0cb895d5ac3e423
unknown <manthanrathod10@gmail.com'git config --global user.name>
2024-08-05 12:39:19 +0530

    Uploading initial code

---
+++ script.js
@@ -0,0 +1,12 @@
+// function home(){
+//   hidecv();
+//   return 0;
+// }
+
+// function showhome(){
+//   document.getElementById("home_page").style = "display: block;";
+// }
+
+// function hidecv(){
+//   document.getElementById("cv_page").style = "display: none;";
+// }
